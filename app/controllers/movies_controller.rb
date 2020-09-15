1cbab99d410aa49f73a1de0eff00494331246092
EC2 Default User <ec2-user@ip-172-31-62-23.ec2.internal>
2020-09-15 23:23:42 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,27 +12,37 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    if params[:ratings]
-      @checked_ratings = params[:ratings].keys
-    else
-      @checked_ratings = @all_ratings
+    if not params[:ratings]
+      params[:ratings] = session[:params_ratings]
+    end 
+    if not params[:sort]
+      params[:sort] = session[:params_sort]
     end
-    @checked_ratings.each do |rating|
+    if params[:ratings]
+      session[:params_ratings] = params[:ratings]
+      @get_raitings = params[:ratings].keys
+    else
+      @get_raitings = @all_ratings
+    end
+    @get_raitings.each do |rating|
       params[rating] = true
     end
     
     if params[:sort]
+      session[:params_sort] = params[:sort]
       @movies = Movie.order(params[:sort])
     else
-      @movies = Movie.where(:rating => @checked_ratings)
+      @movies = Movie.order(params[:sort]).where(:rating => @get_raitings)
     end
+    
   end
-
+  
   def new
     # default: render 'new' template
   end
 
   def create
+    session.clear
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
@@ -55,9 +65,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-  
-  #def get_raiting
-    
-  #end
   
 end
